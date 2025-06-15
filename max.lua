33ef148180c251efae3d7c7c048327a94dc7b093
boycots563 <glowfitaura.orders@gmail.com>
2025-06-15 22:48:38 +0200

    Update max.lua

--- max.lua
+++ max.lua
@@ -1,1 +1,6 @@
-print("Testovací skript max.lua úspešne spustený!")
+game.StarterGui:SetCore("ChatMakeSystemMessage", {
+    Text = "Testovací skript max.lua úspešne spustený!";
+    Color = Color3.fromRGB(0, 255, 0);
+    Font = Enum.Font.SourceSansBold;
+    FontSize = Enum.FontSize.Size24;
+})

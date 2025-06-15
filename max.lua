6473eee26e39d3ed50f5348994d49cd0e9ea8a89
boycots563 <glowfitaura.orders@gmail.com>
2025-06-15 22:47:09 +0200

    Update max.lua

--- max.lua
+++ max.lua
@@ -1,6 +1,1 @@
-
-local obsah = "Ahoj, toto je testovaci subor vytvoreny cez Lua."
-local cesta = os.getenv("TEMP") .. "\\chujtest.txt"
-
-writefile(cesta, obsah)
-print("Subor bol vytvoreny v:", cesta)
+print("Testovací skript max.lua úspešne spustený!")

570e8e725a0012c5a5eb7b78025889024bdc513b
boycots563 <glowfitaura.orders@gmail.com>
2025-06-15 23:05:37 +0200

    Delete max.lua

--- max.lua
+++
@@ -1,6 +0,0 @@
-game.StarterGui:SetCore("ChatMakeSystemMessage", {
-    Text = "Testovací skript max.lua úspešne spustený!";
-    Color = Color3.fromRGB(0, 255, 0);
-    Font = Enum.Font.SourceSansBold;
-    FontSize = Enum.FontSize.Size24;
-})

